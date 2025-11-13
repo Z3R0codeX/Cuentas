f1762a05bd6c95391a1f14b06abc9ae0ffcd321c
J Luis Jimenez <jliusjimenez@gmail.com>
2025-11-12 21:51:58 -0600

    front

--- cuentas-front/src/App.jsx
+++ cuentas-front/src/App.jsx
@@ -2,32 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ItemComponent from './components/ItemComponent.jsx'
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  var X =0;
+
+  //React Hooks
+  let [contador, setContador] = useState(0);
+  let [array, setArray] = useState([]);
+
+  let clickBoton=() =>{
+
+    setContador(++contador);
+    console.log(contador);
+
+    let x={...array,contador};
+    array.push(contador);
+    setArray(array);
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h1>Contador</h1>
+      <p>{contador}</p>
+      <button onClick={clickBoton}>Aumentar</button>
+      {array.map( (item) => (<ItemComponent key={item}/>) )}
+
+      
+
     </>
   )
 }
---
+++ cuentas-front/src/components/ItemComponent.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+export default function ItemComponent() {
+  return (
+    <>
+
+        <h2>Item</h2>
+        <input type="text"/>
+        <Button variant="success">Success</Button>
+
+    </>
+  );
+}
